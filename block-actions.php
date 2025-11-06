52481fcf3b8a76201366e87672700bdb7c02d8b8
David Bowman <dabowman@gmail.com>
2025-11-06 11:33:40 -0700

    Change author

--- block-actions.php
+++ block-actions.php
@@ -5,7 +5,7 @@
  * Version: 1.0.0
  * Requires at least: 6.0
  * Requires PHP: 8.0
- * Author: WPVIP
+ * Author: dabowman
  * License: GPL-2.0-or-later
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain: block-actions
